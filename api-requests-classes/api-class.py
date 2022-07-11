9d8134ebe85cc4e909d23a2966572fd966f87d9e
DAHOTOAC <dahotoac@legal.regn.net>
2022-07-11 15:20:40 +0800

    initial

---
+++ api-requests-classes/api-class.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+class API:
+    def __int__(self, endpoint, method, headers, payload):
+        self.url = ''
+        self.headers ={}
+        self.payload = {}
+    def simpleGet(self):
+        response = requests.request("GET", self.url, headers=self.headers, data=self.payload)
+        result = json.loads(response.text)
+        return result
+    def simplePost(self):
+        response = requests.request("POST", self.url, headers=self.headers, data=self.payload)
+        result = json.loads(response.text)
+        return result
