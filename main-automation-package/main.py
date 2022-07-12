9888b0e09c641b9699e24a49c79610f7479574f7
DAHOTOAC <dahotoac@legal.regn.net>
2022-07-12 09:52:08 +0800

    adding functions

---
+++ main-automation-package/main.py
@@ -0,0 +1,18 @@
+import getpass as mask
+
+def getData():
+    pass
+
+def passData():
+    pass
+def downloadDocument():
+    pass
+
+def main(param):
+    return param
+
+if __name__ == "__main__":
+    apikey = mask.getpass("Enter API Key: ")
+    source_endpoint = input("Enter URL Path of the Source Endpoint: ")
+    destination_endpoint = input("Enter URL Path of the Destination Endpoint: ")
+    print(main("Hello World!"))
